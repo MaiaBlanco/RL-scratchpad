Fix but in lambda weighting for value at end of trace.

TD.py
@@ -22,8 +22,9 @@ class TemporalDifference21:
         # policy (default policy). 
         self._N0 = float(N0)
 
-        self._action_state_counts = np.zeros((10+1, 21+1, 2))
-        self._action_state_values = np.zeros((10+1, 21+1, 2))
+        # Plus one oversizes state space to accomodate for single (-1, -1) terminal state.
+        self._action_state_counts = np.zeros((11, 22, 2))
+        self._action_state_values = np.zeros((11, 22, 2))
 
         if n_steps == 0:
             print("Warning: n_steps must be -1 or greater than 0." \

TD_lambda.py
@@ -91,10 +91,12 @@ class TemporalDifference21_lambd:
                         # Compute the pre-target return (NOTE NO DISCOUNTING):
                         pretarget_return = np.sum(rewards)
                         # Update composite step return:
-                        total_return += (self._lambda**(lookahead-1)) * (pretarget_return + targ_val)
+                        if lookahead == slack-1:
+                            total_return += (self._lambda**(lookahead-1)) * (pretarget_return + targ_val)
+                        else:
+                            total_return += (1 - self._lambda) * (self._lambda**(lookahead-1)) * (pretarget_return + targ_val)
                         
                     # Use composite returns over 1..n steps, old value, and count to update Q value:
                     old_value = Q[d_score_last, p_score_last, last_action]
                     count = C[d_score_last, p_score_last, last_action]
-                    total_return *= (1 - self._lambda)
                     Q[d_score_last, p_score_last, last_action] = old_value + (total_return - old_value) / count

testTD_lambda.py
@@ -5,23 +5,31 @@ import pickle
 
 
 def msqerr(v1, v2):
-	return np.linalg.norm(v1 - v2)
+	return np.linalg.norm(v1 - v2)/(11*22*2.0)
 
-
+K = 2
 with open('mc.pickle', 'rb') as f:
 	mc_vals = pickle.load(f)._action_state_values
 td_l_vals = []
-msqs = np.zeros((11, 1000))
-for k in range(11):
-	lambd = 0.1*k
+msqs = np.zeros((K, 100000))
+for k in range(K):
+	lambd = 1*k
 	td = TD.TemporalDifference21_lambd(N0=100.0, lambd=lambd)
-	for i in range(1000):
+	for i in range(100000):
 		td.run_episode()
 		msqs[k,i] = msqerr(mc_vals, td._action_state_values)
 		td_l_vals.append( (td._action_state_values, td._action_state_counts) )
-	plt.plot(msqs[k,:])
-	plt.title("MSQ Err for Lambda = {}".format(0.1*k))
-	plt.show()
+	if k == 0 or k == K-1:
+		plt.plot(msqs[k,:], label="Lambda {}".format(k*1.0))
+plt.title("MSQ Err over Lambda".format(lambd))
+plt.legend()
+plt.ylabel("Mean Squared Error")
+plt.xlabel("Episode Number")
+plt.show()
+# plt.plot(np.arange(K)*0.1, msqs[:,-1])
+# plt.xlabel("Lambda")
+# plt.ylabel("MSQ Err")
+# plt.show()
 
 # Lambda = 0
 Q, C = td_l_vals[0]